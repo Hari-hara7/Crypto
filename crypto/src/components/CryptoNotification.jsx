ede54389c075197a634955eb3faf6282b3a7de88
Your Name <Hariharanath247@gmail.com>
2025-01-24 21:45:32 +0530

    hari

--- crypto/src/components/CryptoNotification.jsx
+++ crypto/src/components/CryptoNotification.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Replace with your backend URL
+const socket = io("https://crypto-i38w.onrender.com"); 
 
 const CryptoNotification = () => {
   const [cryptoData, setCryptoData] = useState({});
