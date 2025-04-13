1075d80223d63ff12fe976d1044efc031b3f9f53
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2025-04-13 21:33:10 +0530

    Update apolloClient.ts

--- crypto/src/apolloClient.ts
+++ crypto/src/apolloClient.ts
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000', // Make sure this points to your GraphQL server
+    uri: 'http://localhost:4000', 
   }),
   cache: new InMemoryCache(),
 });
