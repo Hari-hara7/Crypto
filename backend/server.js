933272151150694fcb04bc1a14444bf20365fa32
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2025-04-13 16:45:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // Allow all origins for development
+    origin: "*",
   },
 });
 
 const PORT = process.env.PORT || 5000;
 
-// Enable CORS
+
 app.use(cors());
 
-// Function to fetch cryptocurrency data
+
 const fetchCryptoData = async () => {
   try {
     const response = await axios.get(
@@ -31,11 +31,11 @@ const fetchCryptoData = async () => {
   }
 };
 
-// Handle socket.io connections
+
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  // Send crypto data every 10 seconds
+
   const interval = setInterval(async () => {
     const cryptoData = await fetchCryptoData();
     if (cryptoData) {
@@ -43,19 +43,18 @@ io.on("connection", (socket) => {
     }
   }, 10000);
 
-  // Clean up on disconnect
   socket.on("disconnect", () => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
 });
 
-// Default route
+
 app.get("/", (req, res) => {
   res.send("Crypto Notifications Backend is running.");
 });
 
-// Start the server
+
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
