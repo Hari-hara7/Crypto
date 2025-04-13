fae944ec83a6b8e1da65deb2a700b3bd57b224e9
HARIHARANATH <154905590+Hari-hara7@users.noreply.github.com>
2025-04-13 16:44:38 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
-// src/index.js
+
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 
-// Create an instance of ApolloServer with schema and resolvers
 const server = new ApolloServer({
   typeDefs,
   resolvers,
